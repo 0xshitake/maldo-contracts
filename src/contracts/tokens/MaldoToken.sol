35215ce5dd6d1a43a6d1923bb80b024343f604da
0xshitake <0xshitake@proton.me>
2025-04-08 22:12:50 -0400

    chore: mv token file

--- src/contracts/tokens/MaldoToken.sol
+++ src/contracts/tokens/MaldoToken.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.13;
 
 import {ERC20} from "@solady/tokens/ERC20.sol";
 
-/// @title HestiaToken
+/// @title MaldoToken
 /// @notice Currently unused.
-contract HestiaToken is ERC20 {
+contract MaldoToken is ERC20 {
     constructor() {}
 
     function name() public pure override returns (string memory _name) {
